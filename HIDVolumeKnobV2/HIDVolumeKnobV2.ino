3586eea8b384443cc22a4d17e56c8090bd0a87b8
Thomas COIN <esvcorp@gmail.com>
2017-05-01 14:33:26 +0200

    Use 128*64 OLED screen

--- HIDVolumeKnobV2/HIDVolumeKnobV2.ino
+++ HIDVolumeKnobV2/HIDVolumeKnobV2.ino
@@ -32,7 +32,7 @@ void doEncoder_Expanded();
 void doEncoder();
 void doClick();
 
-#if (SSD1306_LCDHEIGHT != 32)
+#if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
